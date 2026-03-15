d16a24a3ccc509ef20f441e2edbe5c99a5057c60
exerinity <me@ex3.lol>
2026-03-15 20:00:41 +1100

    m

--- files/js/stars.js
+++ files/js/stars.js
@@ -210,12 +210,12 @@ document.addEventListener('visibilitychange', function () {
 window.addEventListener('blur', setUnfocused);
 window.addEventListener('focus', setFocused);
 
-// Completely unrelated to stars, but this is for domains: find site-brand, find examples of "exerinity.gay" (the old domain), and make it window.location.hostname
+// Completely unrelated to stars, but this is for domains: find site-brand, find examples of "exerinity.com" (the old domain), and make it window.location.hostname
 document.querySelectorAll(".site-brand").forEach(el => {
-    el.innerHTML = el.innerHTML.replace(/exerinity.gay/g, window.location.hostname);
+    el.innerHTML = el.innerHTML.replace(/exerinity.com/g, window.location.hostname);
 
     // and for the title
-    document.title = document.title.replace(/exerinity.gay/g, window.location.hostname);
+    document.title = document.title.replace(/exerinity.com/g, window.location.hostname);
 });
 
 // (because im changing domains up and dont wanna fuck everything up)
